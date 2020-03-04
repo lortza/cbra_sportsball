a17702f84497da40610c2fe5e1d9746dc67a4718
Anne Richardson <richardson.ae@gmail.com>
2020-03-03 19:43:02 -0600

    Enable rake db:migrate from root folder

--- components/app_component/lib/app_component/engine.rb
+++ components/app_component/lib/app_component/engine.rb
@@ -1,5 +1,13 @@
 module AppComponent
   class Engine < ::Rails::Engine
     isolate_namespace AppComponent
+
+    # This enables rake db:migrate to find the migrations located in the engines
+    # and run them against the main app schema
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match(root.to_s+File::SEPARATOR)
+        app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
+      end
+    end
   end
 end
